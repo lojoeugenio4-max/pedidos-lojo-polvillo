dbadd0244a05fd68897751f408b59d5f8c7a2a49
lojoeugenio4-max <lojoeugenio4@gmail.com>
2026-05-13 21:14:06 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 p-10">
       <div className="max-w-3xl mx-auto bg-white rounded-2xl shadow-lg p-10">
-        
         <h1 className="text-4xl font-bold text-center mb-10">
           App de Pedidos
         </h1>
@@ -12,15 +13,19 @@ export default function Home() {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-
-          <button className="bg-blue-600 hover:bg-blue-700 text-white text-2xl font-semibold p-10 rounded-2xl">
+          <Link
+            href="/bebidas"
+            className="bg-blue-600 hover:bg-blue-700 text-white text-2xl font-semibold p-10 rounded-2xl text-center"
+          >
             Bebidas
-          </button>
+          </Link>
 
-          <button className="bg-red-600 hover:bg-red-700 text-white text-2xl font-semibold p-10 rounded-2xl">
+          <Link
+            href="/charcuteria"
+            className="bg-red-600 hover:bg-red-700 text-white text-2xl font-semibold p-10 rounded-2xl text-center"
+          >
             Charcutería
-          </button>
-
+          </Link>
         </div>
       </div>
     </main>
